f04971f09b68b79590245f370144655afa4ecee4
DanielGitau <danielgitau3396@gmail.com>
2023-03-16 22:02:01 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
+
 #include <stdio.h>
+
 /**
- * Main - Entry point
- *
- * Return: Always 0 (success)
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (success)
  */
 int main(void)
 {
-/* main function */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

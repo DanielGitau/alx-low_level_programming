f382aa7f6b4b8e6aa1aa8d467a66aa8340bcfc2c
DanielGitau <danielgitau3396@gmail.com>
2023-03-22 02:27:37 +0000

    Sum of even Fibonacci terms

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,7 +15,7 @@ int main(void)
 	k = 2;
 	sum = 0;
 
-	for (i = 1; i <= 33; ++1)
+	for (i = 1; i <= 33; ++i)
 	{
 		if (j < 4000000 && (j % 2) == 0)
 		{
@@ -25,6 +25,6 @@ int main(void)
 		j = k;
 		k = next;
 	}
-	printf("%\n", sum);
+	printf("%lu\n", sum);
 	return (0);
 }

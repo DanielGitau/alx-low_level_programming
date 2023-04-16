085f9f68c9fadfa96404cb8236000fbe9fbb7222
DanielGitau <danielgitau3396@gmail.com>
2023-04-16 20:35:53 +0000

    Malloc

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs - Calculate the absolute value of an integer
+ *@n: integer to be calcualted
+ *Return: absolute value of integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}

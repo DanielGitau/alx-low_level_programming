d2e80c573632e9fb80cb9e480bfd540469aa610f
DanielGitau <danielgitau3396@gmail.com>
2023-04-27 23:34:17 +0000

    Adding 1-list_len.c file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *list_len - returns the number
+ *of elements in alinked list
+ *@h: It is a pointer to list_t
+ *Return: number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t k = 0;
+
+	while (h)
+	{
+		k++;
+		h = h->next;
+	}
+	return (n);
+}

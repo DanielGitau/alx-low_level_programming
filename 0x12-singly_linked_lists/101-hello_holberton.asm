9a270f79498b0575faf0dc7fac37db971f1eff50
DanielGitau <danielgitau3396@gmail.com>
2023-04-28 01:31:28 +0000

    Assembly language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,18 @@
 section .data
-  hello db 'Hello, Holberton', 10, 0
-  
-section .text
-  global main
-  extern printf
+    format db 'Hello, Holberton', 10, 0
 
-main:
-  push hello
-  call printf
-  add rsp, 8
-  xor eax, eax
-  ret
+section .text
+    global _start
+
+_start:
+    ; write the string to stdout
+    mov eax, 1          ; system call number for write
+    mov edi, 1          ; file descriptor for stdout
+    mov rsi, format     ; address of the string to write
+    mov edx, 16         ; number of bytes to write
+    syscall
+
+    ; exit the program
+    mov eax, 60         ; system call number for exit
+    xor edi, edi        ; exit code 0
+    syscall

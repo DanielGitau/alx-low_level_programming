5f13a305f046d3ca17ba947bdf5dc966804a6b22
DanielGitau <danielgitau3396@gmail.com>
2023-04-28 01:17:08 +0000

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 
+void printBeforemain(void) __attribute__ ((constructor));
+
 /**
  * printBeforemain - Prints a sentence before the main
  * function is executed
  */
 
-void printBeforemain()
+void printBeforemain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");

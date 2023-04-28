4fe30ce741c5745637ba96363ebe42bede191c63
DanielGitau <danielgitau3396@gmail.com>
2023-04-28 00:56:15 +0000

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * printBeforemain - Prints a sentence before the main
+ * function is executed
+ */
+
+void printBeforemain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

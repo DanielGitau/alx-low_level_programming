3dfeebdc181a3dfdde0ebd2277133f8beaf76697
DanielGitau <danielgitau3396@gmail.com>
2023-05-01 07:40:36 +0000

    Updating 0-print_listint.c file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	do {
+	while (h)
+	{
 		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
-	while (h)
 	return (count);
 }
 

fc875253328c7e5fceed99c45bbfef9ad8270421
DanielGitau <danielgitau3396@gmail.com>
2023-05-01 07:57:13 +0000

    1-listint_len.c file

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.c"
+
+/**
+ * listint_len - Returns the no. of elements
+ * in a linked list
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	for (: h != NULL; h = h->next)
+	{
+		count++;
+	}
+	return (count);
+}

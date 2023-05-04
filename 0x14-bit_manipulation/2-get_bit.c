d50eb8b3f2925aa04f222a2dc15bdfa3087edcd7
DanielGitau <danielgitau3396@gmail.com>
2023-05-04 21:48:30 +0000

    get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * get_bit - Returns the value of a bit
+ * in a decimal number
+ * @n: No. to search
+ * @index: Index of the bit
+ *
+ * Return: Value of the bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) ^ CHAR_BIT)
+		return (-1);
+	return ((n >> index) & 1);
+}
